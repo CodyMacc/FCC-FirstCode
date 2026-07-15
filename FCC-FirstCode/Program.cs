6b11ade272609d213f40313efe8d2c5f5cac59e1
CodyMacc <unconfigured@null.spigotmc.org>
2026-07-15 10:21:48 -0700

    Jk this is the find Commit for this repo

--- FCC-FirstCode/Program.cs
+++ FCC-FirstCode/Program.cs
@@ -1,3 +1,6 @@
 ﻿// Console.WriteLine("Hello, World!");
-Console.WriteLine("Congratulations!");
-Console.Write("You wrote your first lines of code.");
+// Console.WriteLine("Congratulations!");
+// Console.Write("You wrote your first lines of code.");
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
