94aa01ff3067bbbfe8b412a9271edc86210af695
CodyMacc <unconfigured@null.spigotmc.org>
2026-07-15 10:11:10 -0700

    Removed space and made it a new line. Final edit for this code.

--- FCC-FirstCode/Program.cs
+++ FCC-FirstCode/Program.cs
@@ -1,4 +1,3 @@
 ﻿// Console.WriteLine("Hello, World!");
-Console.Write("Congratulations!");
-Console.Write(" ");
+Console.WriteLine("Congratulations!");
 Console.Write("You wrote your first lines of code.");
